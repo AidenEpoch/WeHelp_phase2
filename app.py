4dccaa8fa73f25230ec6705421e823f8721aa45f
AidenEpoch <AidenEpoch1217@gmail.com>
2026-08-15 11:48:51 +0800

    import os 並且將靜態掛載的網址改成絕對網址

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import *
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import os
 app=FastAPI()
 
 
@@ -212,4 +213,8 @@ async def getAttractionById(attraction_id: int):
             conn.close()
 
     
-    app.mount("/static", StaticFiles(directory="static"), name="static")
+    app.mount(
+    "/static",
+    StaticFiles(directory=os.path.join(BASE_DIR, "static")),
+    name="static",
+)
